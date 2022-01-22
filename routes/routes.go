0594703edf6ee262408fb46e7949c54aeb7dcf8f
Leonardo T. Fernandes <leo.80@protonmail.com>
2022-01-22 10:47:17 -0300

    feat: add new routes and reorganize the file

--- routes/routes.go
+++ routes/routes.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"example.com/go-poc/controllers"
+	"example.com/go-poc/middlewares"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
@@ -13,7 +14,29 @@ import (
 func HandleRequest(port string) {
 	fmt.Printf("Server starting on port: " + port + "\n")
 	r := chi.NewRouter()
+
+	// Middlewares
 	r.Use(middleware.Logger)
-	r.Get("/", controllers.GetAllBooks)
+	r.Use(middlewares.ContentTypeMiddleware)
+
+	// Prefix
+	r.Route("/api", func(r chi.Router) {
+
+		// Books
+		r.Route("/books", func(r chi.Router) {
+			r.Get("/", controllers.GetAllBooks)
+			r.Get("/{id}", controllers.GetBookById)
+			r.Post("/", controllers.CreateBook)
+			r.Delete("/{id}", controllers.DeleteBook)
+		})
+
+		// Authors
+		r.Route("/authors", func(r chi.Router) {
+			r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("authors"))
+			})
+		})
+	})
+
 	http.ListenAndServe(":" + port, r)
 }
